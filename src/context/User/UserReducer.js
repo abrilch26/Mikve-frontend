76d72aa8e133e99ad2a3db027adf6eff5cc3c45b
Abril Chávez Romero <abril_ch26@hotmail.com>
2021-12-21 15:44:56 -0600

    User context

---
+++ src/context/User/UserReducer.js
@@ -0,0 +1,43 @@
+const reducer = (globalState, action) => {
+    switch (action.type) {
+
+        case "LOGIN_SUCCEED":
+        case "REGISTER_SUCCEED":
+            localStorage.setItem("token", action.payload);
+
+            return {
+                ...globalState,
+                authStatus: true,
+            };
+
+        case "REGISTER_FAIL":
+            return {
+                ...globalState,
+                authStatus: false,
+                msg: action.payload,
+            }
+
+        case "GET_DATA_USER":
+            return {
+                ...globalState,
+                authStatus: true,
+                currentUser: action.payload,
+            }
+
+        case "LOGOUT_USUARIO":
+            localStorage.removeItem("token");
+
+            return {
+                ...globalState,
+                user: null,
+                authStatus: null,
+                msg: action.payload
+            }
+
+        default:  
+        return globalState;
+       
+    }
+}
+
+export default reducer;
--- src/context/User/UserState.js
+++ src/context/User/UserState.js
@@ -8,14 +8,105 @@ const UserState = (props) => {
     const initialState = {
         currentUser: {
             nombre: "",
-		apellido: "",
-		telefono: "",
-		pais: "",
-		email: "",
-		userImage: "",
-		bio: "",
-		password: "",
-		owner: ""
+            apellido: "",
+            telefono: "",
+            pais: "",
+            email: "",
+            userImage: "",
+            bio: "",
+            password: "",
+            purchasedProducts: []
+        },
+        authStatus: false,
+        msg: "",
+    }
+
+    const [globalState, dispatch] = useReducer(UserReducer, initialState)
+
+
+
+    const registerUser = async (form) => {
+        const res = await axiosClient.post("users/signup", form)
+        const token = res.data.data
+        const msg = res.data.msg
+
+        if (msg) {
+            dispatch({
+                type: "REGISTER_FAIL",
+                payload: msg
+            })
+        } else {
+            dispatch({
+                type: "REGISTER_SUCCEED",
+                payload: token
+            })
         }
     }
+
+
+    const loginUser = async (form) => {
+        const res = await axiosClient.post("users/login", form)
+        const token = res.data.data
+
+        dispatch({
+            type: "LOGIN_SUCCEED",
+            payload: token
+        })
+    }
+
+    const verifyingToken = async () => {
+        const token = localStorage.getItem("token")
+        //anexar token a la peticion de axios
+        if (token) {
+            axiosClient.defaults.headers.common["x-auth-token"] = token
+        } else {
+            delete axiosClient.defaults.headers.common["x-auth-token"]
+        }
+
+        try {
+            const res = await axiosClient.get("users/verifytoken")
+            const userData = res.data.data
+
+            dispatch({
+                type: "GET_DATA_USER",
+                payload: userData
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    const logoutUser = async () => {
+        dispatch({
+            type: "LOGOUT_USER",
+        })
+    }
+
+
+    const editProfile = async (form, idUser) => {
+        const res = await axiosClient.put(`users/editprofile/${idUser}`, form);
+    };
+
+    return (
+        <UserContext.Provider
+            value={{
+                currentUser: globalState.currentUser,
+                authStatus: globalState.authStatus,
+                msg: globalState.msg,
+                purchasedProducts: globalState.purchasedProducts,
+                registerUser,
+                loginUser,
+                verifyingToken,
+                logoutUser,
+                editProfile
+            }}
+        >
+            {props.children}
+        </UserContext.Provider>
+    )
 }
+
+
+export default UserState
